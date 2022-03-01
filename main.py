7495e7d20d2c3d40f6ec40ed44bcbe2013537854
akhil451 <akhilprasannan451@gmail.com>
2022-03-01 17:15:51 +0530

    pexels code completed and tested.

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-import requests
-import ast
-import json
-import urllib.request
+### refer this ---> https://gist.github.com/namoopsoo/154c97ef4fa0d36a4daffeeb0d1affd5
+
 import os
+import sys
+import argparse
+import logging
+from utils.file_utils import check_search_file,get_classes_to_download
+from gooey import Gooey
+from pexels.pexels import download_pexel_images
 
-def get_image_links(category,n_images):
-    response = requests.get("https://api.pexels.com/v1/search?query="+category+"&per_page=10",headers= my_headers)
-    json_data = json.loads(response.text)
-#     print( json_data['photos'][0]['src']['large'])
-    image_links= []
-    for i in json_data['photos']:
-#         print(i)
-        image_link = i['src']['large']
-#         print(image_link)
-        image_links.append(image_link)
-    return image_links
+# @Gooey        
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
 
-def download_images(category,image_links):
-    if not os.path.exists("temp/data/"+category):
-        os.makedirs("temp/data/"+category)
-    for image_link in image_links:
-#         urllib.request.urlretrieve(image_link, os.path.join("temp/data/"+category,image_link.rsplit("/",1)[-1]))
-        image_path = os.path.join("temp/data/"+category,image_link.rsplit("/",1)[-1].rsplit("?",1)[0])
-        response = requests.get(image_link, stream=True)
-        with open(image_path, 'wb') as outfile:
-            outfile.write(response.content)
+    parser.add_argument('--use_pexels',action='store_true')
+    parser.add_argument("--images_per_class",type=int,default=50)
+    parser.add_argument('--search_file',default="user_files/classes_to_search.txt")
+    parser.add_argument('--output_location', default = "user_files/temp")
+    parser.add_argument('--pexels_auth_code', default = "")
+
+
+    args = parser.parse_args()
+    # print(args.use_pexels)
+    if args.use_pexels:
+        assert not args.pexels_auth_code==None
+        assert check_search_file(args.search_file) == True
+        if not os.path.exists(args.output_location):
+            os.makedirs(args.output_location)
+        categories =  get_classes_to_download(args.search_file)
+        print("categories",categories)
+        download_pexel_images(auth_code=args.pexels_auth_code,output_loc=\
+            args.output_location,n_images_per_class=args.images_per_class,categories=categories)
+if __name__ == "__main__":
+
+    main()
--- pexels/pexels.py
+++ pexels/pexels.py
@@ -4,13 +4,14 @@ import json
 import urllib.request
 import os
 import itertools
-from ignore import authcode
+# from ignore import authcode
 
 def get_image_links(category:str,n_images:int,authcode:str):
     my_headers = {'Authorization' : authcode}
     response = requests.get("https://api.pexels.com/v1/search?query="+category+"&per_page="+str(n_images),headers= my_headers)
     json_data = json.loads(response.text)
     image_links= []
+    print(json_data)
     for i in json_data['photos']:
         try:
             image_link = i['src']['original']
@@ -19,7 +20,7 @@ def get_image_links(category:str,n_images:int,authcode:str):
              print(error)
     return image_links
 
-def download_image(output_loc:str,category:str,image_link:str):
+def download_pexel_image(output_loc:str,category:str,image_link:str):
     if not os.path.exists(os.path.join(output_loc,category)):
         os.makedirs(os.path.join(output_loc,category))
     image_path = os.path.join(output_loc,category,image_link.rsplit("/",1)[-1].rsplit("?",1)[0])
@@ -27,7 +28,7 @@ def download_image(output_loc:str,category:str,image_link:str):
     with open(image_path, 'wb') as outfile:
         outfile.write(response.content)
 
-def download_pexel_images(auth_code:str,output_loc:str,n_images_per_class:int,categories:str):
+def download_pexel_images(auth_code:str,output_loc:str,n_images_per_class:int,categories:ast.List):
     """
     Accumulate all image links at one place. To incorporate multiprocessing later
     """
@@ -38,11 +39,11 @@ def download_pexel_images(auth_code:str,output_loc:str,n_images_per_class:int,ca
             all_image_links.append(image_links)
         flat_list = list(itertools.chain(*all_image_links))
         for image_link in flat_list:
-            download_image(output_loc,image_link[1],image_link[0])
+            download_pexel_image(output_loc,image_link[1],image_link[0])
     except Exception as error:
         print(error)
 
-if __name__ == "__main__":
-    download_pexel_images(auth_code=authcode,output_loc="temp",n_images_per_class=10,categories=['car','shoes'])
+# if __name__ == "__main__":
+    # download_pexel_images(auth_code=authcode,output_loc="temp",n_images_per_class=10,categories=['car','shoes'])
 
 
